92f60510da662db730bad6f3f1ae7f5a6315719a
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-09-22 15:43:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,2 @@
-def consolidate_cart(cart)
-  # code here
-end
+require 'pry'
 
-def apply_coupons(cart, coupons)
-  # code here
-end
-
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
-  # code here
-end
